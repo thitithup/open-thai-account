7605a586b6e8c2d05f94c2e6a83c91200a068107
tititab <thitithup@gmail.com>
2023-02-21 11:26:08 +0700

    New module : ineco_account_base (เพิ่มฟิล์ดสำหรับตั้งค่า Chart of Account)

---
+++ ineco_account_base/models/account_account.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _, tools
+from odoo.osv import expression
+from odoo.exceptions import UserError, ValidationError
+
+
+class AccountAccount(models.Model):
+    _inherit = 'account.account'
+    _order = 'code'
+
+    parent_id = fields.Many2one('account.account', string='Parent')
+    name2 = fields.Char(string=u'Secondary Name', copy=False, track_visibility=True)
+    tax_sale_ok = fields.Boolean(string=u'ภาษีขาย', copy=False, track_visibility=True)
+    tax_purchase_ok = fields.Boolean(string=u'ภาษีซื้อ', copy=False, track_visibility=True)
+    cheque_in_ok = fields.Boolean(string=u'เช็ครับ', copy=False, track_visibility=True)
+    cheque_out_ok = fields.Boolean(string=u'เช็คจ่าย', copy=False, track_visibility=True)
+    deposit_ok = fields.Boolean(string=u'มัดจำ', copy=False, track_visibility=True)
+    wht_purchase_ok = fields.Boolean(string=u'ภาษีหัก ณ ที่จ่าย', copy=False, track_visibility=True)
+    wht_sale_ok = fields.Boolean(string=u'ภาษีถูกหัก ณ ที่จ่าย', copy=False, track_visibility=True)
+    wait = fields.Boolean(string=u'ภาษีซื้อรอนำส่ง', copy=False, track_visibility=True)
+
+
+class AccountAccountTemplate(models.Model):
+    _inherit = 'account.account.template'
+
+    parent_id = fields.Many2one('account.account.template', string='Parent')
